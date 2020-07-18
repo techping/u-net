ddf6903c5883915afa0995c8e6ab2a86ab2f23b3
Ziping Chen <techping.chan@gmail.com>
2020-07-18 15:43:54 -0700

    [ziping/model] modify model for faster convergence and higher performance
    Signed-off-by: Ziping Chen <techping.chan@gmail.com>

--- model.py
+++ model.py
@@ -4,12 +4,15 @@
 
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 def double_conv(in_c, out_c):
     return nn.Sequential(
         nn.Conv2d(in_c, out_c, kernel_size=3),
+        nn.BatchNorm2d(out_c),
         nn.ReLU(inplace=True),
         nn.Conv2d(out_c, out_c, kernel_size=3),
+        nn.BatchNorm2d(out_c),
         nn.ReLU(inplace=True),
     )
 
@@ -22,18 +25,32 @@ def upconv(in_c, out_c):
         )
 
 def crop(in_tensor, out_tensor):
-    diff = (in_tensor.size()[2] - out_tensor.size()[2]) // 2
-    return in_tensor[:, :, diff:-diff, diff:-diff]
+    diff_h = (in_tensor.size()[2] - out_tensor.size()[2])
+    left_h = diff_h // 2
+    diff_w = (in_tensor.size()[3] - out_tensor.size()[3])
+    left_w = diff_w // 2
+    return in_tensor[:, :, left_h:-(diff_h-left_h), left_w:-(diff_w-left_w)]
+
+def pad(in_tensor, out_tensor):
+    diff_h = (in_tensor.size()[2] - out_tensor.size()[2])
+    left_h = diff_h // 2
+    diff_w = (in_tensor.size()[3] - out_tensor.size()[3])
+    left_w = diff_w // 2
+    return F.pad(out_tensor, [left_w, diff_w - left_w,
+                                left_h, diff_h - left_h])
+
 
 class UNet(nn.Module):
-    def __init__(self):
+    def __init__(self, in_channels=1, num_classes=2):
         super(UNet, self).__init__()
+        self.in_channels = in_channels
+        self.num_classes = num_classes
         self.contracting_path()
         self.expansive_path()
 
     def contracting_path(self):
         self.max_pooling = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.down_conv_1 = double_conv(1, 64)
+        self.down_conv_1 = double_conv(self.in_channels, 64)
         self.down_conv_2 = double_conv(64, 128)
         self.down_conv_3 = double_conv(128, 256)
         self.down_conv_4 = double_conv(256, 512)
@@ -48,9 +65,10 @@ class UNet(nn.Module):
         self.down_conv_7 = double_conv(512, 256)
         self.down_conv_8 = double_conv(256, 128)
         self.down_conv_9 = double_conv(128, 64)
-        self.conv_1x1 = nn.Conv2d(in_channels=64, out_channels=2, kernel_size=1)
+        self.conv_1x1 = nn.Conv2d(in_channels=64, out_channels=self.num_classes, kernel_size=1)
 
     def forward(self, image):
+        input_shape = image.shape[2:]
         # encoder
         x1 = self.down_conv_1(image)
         x2 = self.max_pooling(x1)
@@ -63,24 +81,32 @@ class UNet(nn.Module):
         x9 = self.down_conv_5(x8)
         # decoder
         x = self.up_conv_1(x9)
-        x = torch.cat([crop(x7, x), x], 1)
+        x = torch.cat([x7, pad(x7, x)], 1)
         x = self.down_conv_6(x)
         x = self.up_conv_2(x)
-        x = torch.cat([crop(x5, x), x], 1)
+        x = torch.cat([x5, pad(x5, x)], 1)
         x = self.down_conv_7(x)
         x = self.up_conv_3(x)
-        x = torch.cat([crop(x3, x), x], 1)
+        x = torch.cat([x3, pad(x3, x)], 1)
         x = self.down_conv_8(x)
         x = self.up_conv_4(x)
-        x = torch.cat([crop(x1, x), x], 1)
+        x = torch.cat([x1, pad(x1, x)], 1)
         x = self.down_conv_9(x)
         x = self.conv_1x1(x)
-        print(x.size())
+        x = F.interpolate(x, size=input_shape, mode="bilinear", align_corners=False)
         return x
 
 
 if __name__ == '__main__':
-    # test
-    fake_input = torch.rand((1, 1, 572, 572))
-    model = UNet()
-    model(fake_input)
+    # from torchviz import make_dot, make_dot_from_trace
+
+    # # test
+    # fake_input = torch.rand((1, 3, 572, 572))
+    # model = UNet(3, 1)
+    # # output = model(fake_input)
+    # # print(output.shape)
+
+    # dot = make_dot(model(fake_input), params=dict(model.named_parameters()))
+    # dot.format = 'png'
+    # dot.render("old")
+    pass
